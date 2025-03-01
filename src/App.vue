4f9a5dddb2e1ad794b791df2ad60dd5cabeb58f5
collectivecloudperu <collectivecloudperu@gmail.com>
2025-03-01 13:49:36 -0500

    Cambios

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Editor from "@tinymce/tinymce-vue";
+</script>
+
+<template>
+  <main id="sample">
+    <Editor
+      api-key="ACA-TU-API-KEY"
+      :init="{
+        plugins: 'lists link image table code help wordcount'
+      }"
+    />
+  </main>
+</template>
+
+<style scoped>
+#sample{
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  width: 1000px;
+}
+</style>
